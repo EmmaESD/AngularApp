ed273a252ec18ac86c61ec360493ca9372885ad8
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-01-08 16:18:23 +0100

    chapitre 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+let heroCount = 2;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
+  title = 'la tour du héros';
+  titleStyle = { 'font-family': 'Arial', 'font-size': '2rem' };
+  username = 'Anonymous';
+  heroes = ['Batman', 'Superman', 'Wonderwoman', 'Ironman'];
+  heroToAdd = '';
+  addHero() {
+    this.heroes.push(this.heroToAdd);
+    this.title = 'La tour des ' + this.heroes.length + ' héros';
+  }
 }
